f138afa8cdbbd9fc506f4bb171d92573fb1a1731
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-02-01 15:30:10 +0100

    Stop fixer from requiring docs dir

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,13 +5,7 @@ declare(strict_types=1);
 use Duon\Development\PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
-$paths = [__DIR__ . '/src', __DIR__ . '/tests'];
-
-if (is_dir(__DIR__ . '/docs')) {
-	$paths[] = __DIR__ . '/docs';
-}
-
-$finder = Finder::create()->in($paths);
+$finder = Finder::create()->in([__DIR__ . '/src', __DIR__ . '/tests']);
 $config = new Config();
 
 return $config->setFinder($finder);
