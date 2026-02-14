4c4b15ba71a361d291bf2810e416023f0eb22bbe
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-02-14 22:25:30 +0100

    Add NodeMeta service as caching facade for Meta
    Static service with forClass(), kind(), handle(), name(), route(),
    titleField(), fieldOrder(), deletable() methods. Caches Meta instances
    per class to replace the Node::$nodeMeta static array.

---
+++ src/Node/NodeMeta.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Duon\Cms\Node;
+
+class NodeMeta
+{
+	/** @var array<class-string, Meta> */
+	private static array $cache = [];
+
+	/**
+	 * @param class-string $class
+	 */
+	public static function forClass(string $class): Meta
+	{
+		return self::$cache[$class] ??= new Meta($class);
+	}
+
+	/**
+	 * @param class-string $class
+	 */
+	public static function kind(string $class): string
+	{
+		return self::forClass($class)->kind;
+	}
+
+	/**
+	 * @param class-string $class
+	 */
+	public static function handle(string $class): string
+	{
+		return self::forClass($class)->handle;
+	}
+
+	/**
+	 * @param class-string $class
+	 */
+	public static function name(string $class): string
+	{
+		return self::forClass($class)->name;
+	}
+
+	/**
+	 * @param class-string $class
+	 */
+	public static function route(string $class): string|array
+	{
+		return self::forClass($class)->route;
+	}
+
+	/**
+	 * @param class-string $class
+	 */
+	public static function titleField(string $class): ?string
+	{
+		return self::forClass($class)->titleField;
+	}
+
+	/**
+	 * @param class-string $class
+	 */
+	public static function fieldOrder(string $class): ?array
+	{
+		return self::forClass($class)->fieldOrder;
+	}
+
+	/**
+	 * @param class-string $class
+	 */
+	public static function deletable(string $class): bool
+	{
+		return self::forClass($class)->deletable;
+	}
+
+	public static function clearCache(): void
+	{
+		self::$cache = [];
+	}
+}
